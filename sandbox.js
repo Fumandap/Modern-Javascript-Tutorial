19fc453098bc7e3da42b6eee6b225b4a549ec7f2
Francis Umandap <fumandap@academic.rrc.ca>
2023-01-10 15:24:33 -0600

    Writing functions inside objects.

--- sandbox.js
+++ sandbox.js
@@ -5,19 +5,30 @@ let user = {
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'Berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user logged out');
+    },
+    // arrow function doesn't work when using this. May work unexpectedly.
+    // can also write like this, use regular functions when in an object and can use the simplified version.
+    // logBlogs(){
+    logBlogs: function(){
+        //console.log(this.blogs);
+        console.log('this user has written the following blogs: ');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        });
+    }
 };
 
-console.log(user);
-console.log(user.name);
+user.login();
+user.logout();
+user.logBlogs();
 
-user.age = 35;
-console.log(user.age);
+const name = 'mario';
+name.toUpperCase();
+console.log(name);
 
-const key = 'location';
-
-console.log(user[key]);
-user['name'] = 'chun-li';
-console.log(user['name']);
-
-console.log(typeof user);
