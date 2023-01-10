cff8a099ec1e9f4c999c9cc3e718994327d18fcb
Francis Umandap <fumandap@academic.rrc.ca>
2023-01-10 09:26:13 -0600

    Using else if example.

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,11 @@
-// if statements
-const age = 25;
+// else if statements
 
-if(age > 20){
-    console.log('you arte over 20 years old');
-}
+const password = 'p@ssword1234';
 
-const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-if(ninjas.length > 3){
-    console.log('that\'s a lot of ninjas');
-}
-
-const password = 'p@ssword';
-
-if(password.length >= 8){
-    console.log('that password is long enough');
+if(password.length >= 12){
+    console.log('that password is mighty strong');
+} else if(password.length >= 8){
+    console.log('that password is long strong');
+} else {
+    console.log('that password is not long enough');
 }
