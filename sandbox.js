f0dc05007aef260219a95428753269d27b79f63d
Francis Umandap <fumandap@academic.rrc.ca>
2023-01-10 09:21:31 -0600

    Using if statements.

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,18 @@
-// do while loops
+// if statements
+const age = 25;
 
-let i = 0;
+if(age > 20){
+    console.log('you arte over 20 years old');
+}
 
-do{
-    console.log('val of i is: ', i);
-    i++;
-}while(i < 5);
+const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+if(ninjas.length > 3){
+    console.log('that\'s a lot of ninjas');
+}
+
+const password = 'p@ssword';
+
+if(password.length >= 8){
+    console.log('that password is long enough');
+}
