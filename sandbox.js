23a8545c043a5a3f7c8a745abf9350d82fc51210
Francis Umandap <fumandap@academic.rrc.ca>
2023-01-10 15:31:40 -0600

    Using the math object with examples.

--- sandbox.js
+++ sandbox.js
@@ -1,44 +1,19 @@
-// object literals
+// Math object
 
-// const blogs = [
-//     {title: 'why mac & cheese rules', likes: 30},
-//     {title: '10 things to make with marmite', likes: 50}
-// ];
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
 
-// console.log(blogs);
+const area = 7.7;
 
-let user = {
-    name: 'crystal',
-    age: 30,
-    email: 'crystal@thenetninja.co.uk',
-    location: 'Berlin',
-    blogs: [
-        {title: 'why mac & cheese rules', likes: 30},
-        {title: '10 things to make with marmite', likes: 50}
-    ],
-    login: function(){
-        console.log('the user logged in');
-    },
-    logout: function(){
-        console.log('the user logged out');
-    },
-    // arrow function doesn't work when using this. May work unexpectedly.
-    // can also write like this, use regular functions when in an object and can use the simplified version.
-    // logBlogs(){
-    logBlogs: function(){
-        //console.log(this.blogs);
-        console.log('this user has written the following blogs: ');
-        this.blogs.forEach(blog => {
-            console.log(blog.title, blog.likes);
-        });
-    }
-};
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
 
-user.login();
-user.logout();
-user.logBlogs();
+// random numbers
 
-const name = 'mario';
-name.toUpperCase();
-console.log(name);
+const random = Math.random();
 
+console.log(random);
+console.log(Math.round(random * 100));
