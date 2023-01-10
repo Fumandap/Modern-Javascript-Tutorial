ba3f0104ec766bf0b50addfa41aa7979b980c7ad
Francis Umandap <fumandap@academic.rrc.ca>
2023-01-10 09:14:13 -0600

    Using a do while loop.

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,8 @@
-// while loops
-
-const names = ['shaun', 'mario', 'luigi'];
-// let i = 0;
-
-// while(i < 5){
-//     console.log('in loop: ', i);
-
-//     i++;
-// }
+// do while loops
 
 let i = 0;
-while(i < names.length){
-    console.log(names[i]);
+
+do{
+    console.log('val of i is: ', i);
     i++;
-}
+}while(i < 5);
