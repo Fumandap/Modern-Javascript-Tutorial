bc2baffaf015fd9836a4abf2373b85eb3048f748
Francis Umandap <fumandap@academic.rrc.ca>
2023-01-10 15:13:07 -0600

    Using object literals and accessing.

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,23 @@
-// get a reference to the 'ul'
-const ul = document.querySelector('.people');
+// object literals
 
-const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'crystal@thenetninja.co.uk',
+    location: 'Berlin',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+};
 
-let html = ``;
+console.log(user);
+console.log(user.name);
 
-people.forEach(person => {
-    html += `<li style ="color: purple">${person}</li>`;
-});
+user.age = 35;
+console.log(user.age);
 
-console.log(html);
+const key = 'location';
 
-ul.innerHTML = html;
+console.log(user[key]);
+user['name'] = 'chun-li';
+console.log(user['name']);
+
+console.log(typeof user);
