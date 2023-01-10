e3fca2d14d14b7bec6799593e2ee81dfa8f64660
Francis Umandap <fumandap@academic.rrc.ca>
2023-01-10 09:44:18 -0600

    Looking at variables and block scope.

--- sandbox.js
+++ sandbox.js
@@ -1,22 +1,15 @@
-// switch statements
-const grade = 'D';
+// variables and block scope
+let age = 30;
 
-switch(grade){
-    case 'A':
-        console.log('you got an A!');
-        break;
-    case 'B':
-        console.log('you got an B!');
-        break;
-    case 'C':
-        console.log('you got an C!');
-        break;
-    case 'D':
-        console.log('you got an D!');
-        break;
-    case 'E':
-        console.log('you got an E!');
-        break;
-    default:
-        console.log('not a valid grade.');
+if(true){
+    let age = 40;
+    let name = 'shaun';
+    console.log('inside code block: ', age, name);
+    if(true){
+        let age = 50;
+        console.log('inside 2nd code block: ', age);
+        var test = 'hello';
+    }
 }
+
+console.log('outside code block: ', age, name, test);
