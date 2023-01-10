e23183570a4f479aa6f8097eacff5c6d9c201c50
Francis Umandap <fumandap@academic.rrc.ca>
2023-01-10 09:52:18 -0600

    Working with functions.

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,14 @@
-// variables and block scope
-let age = 30;
-
-if(true){
-    let age = 40;
-    let name = 'shaun';
-    console.log('inside code block: ', age, name);
-    if(true){
-        let age = 50;
-        console.log('inside 2nd code block: ', age);
-        var test = 'hello';
-    }
+// function declaration
+function greet(){
+    console.log('hello there');
 }
 
-console.log('outside code block: ', age, name, test);
+// function expression
+// this way of declaring a function will be used.
+const speak = function(){
+    console.log('good day!');
+};
+
+greet();
+
+speak();
